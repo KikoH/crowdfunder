2693c82d5af465bfc551947aa16d2c0cb2a6f4bb
Jude Fiorillo <jude.fiorillo@gmail.com>
2014-12-01 18:44:24 -0500

    create pledge model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141201234318) do
+ActiveRecord::Schema.define(version: 20141201234400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: true do |t|
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pledges", force: true do |t|
+    t.integer  "contribution"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
